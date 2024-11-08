c89ed09dbdc2b26463f122221fdf2f51a8f89324
firazputraa <mfirazpsa.2004@gmail.com>
2024-11-08 14:30:31 +0700

    Baru buat navbar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        font: '#fff000',
+        nav: '#1e3a8a',
+      },
+      fontFamily:{
+        logo:['Bridone'],
+      }
+    },
+  },
+  plugins: [],
+}
+
