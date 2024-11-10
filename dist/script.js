92828eb094c3a3dbb0107d56711ee2100acff357
firazputraa <mfirazpsa.2004@gmail.com>
2024-11-10 16:57:48 +0700

    landing Page

--- dist/script.js
+++ dist/script.js
@@ -7,6 +7,22 @@ hamburger.addEventListener('click',function(){
     navMenu.classList.toggle('hidden');
 });
 
+// Script to handle dropdown visibility on click
+const aboutButton = document.getElementById('about-button');
+const aboutDropdown = document.getElementById('about-dropdown');
+ aboutButton.addEventListener('click', () => {
+     // Toggle dropdown visibility
+     aboutDropdown.classList.toggle('hidden');
+ });
+ // Close dropdown when clicking outside
+ window.addEventListener('click', (event) => {
+     if (!aboutButton.contains(event.target) && !aboutDropdown.contains(event.target)) {
+         aboutDropdown.classList.add('hidden');
+     }
+ });
+
+
+
 // navbarfixed
 window.onscroll = function(){
     const header = document.querySelector('header');
