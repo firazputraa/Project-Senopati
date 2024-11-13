4b1a1755f0a7d7b230f547c8760ec1956d4ba644
firazputraa <mfirazpsa.2004@gmail.com>
2024-11-13 16:48:15 +0700

    Penambahan animasi scroll

--- dist/script.js
+++ dist/script.js
@@ -6,15 +6,3 @@ hamburger.addEventListener('click',function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
-
-// navbarfixed
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-    }else{
-        header.classList.remove('navbar-fixed');
-    }
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container:{
+      center:true,
+      padding:'16px'
+    },
     extend: {
+      animation:{
+        "loop-scroll": "loop-scroll 40s linear infinite"
+      },
+      keyframes:{
+        "loop-scroll":{
+          from:{transform:"translateX(0)"},
+          to: {transform: "translateX(-100%)"},
+        },
+      },
       colors: {
         font: '#fff000',
         nav: '#1e3a8a',
+        latar: '#3e6b98',
       },
       fontFamily:{
         logo:['Bridone'],
