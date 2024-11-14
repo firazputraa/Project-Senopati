6f7eb18cd8efa34a33993326f3c17e3a0d861586
firazputraa <mfirazpsa.2004@gmail.com>
2024-11-14 19:05:12 +0700

    hahaha

--- dist/script.js
+++ dist/script.js
@@ -7,3 +7,14 @@ hamburger.addEventListener('click',function(){
     navMenu.classList.toggle('hidden');
 });
 
+// navbarfixed 
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+};
